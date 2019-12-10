b6dacfc440b511e33a851859faf313312942ac05
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-10 21:23:12 +0000

    Game class: initialize, set question, ask question, check lives, start game

--- game.rb
+++ game.rb
@@ -2,18 +2,55 @@ require "./player.rb"
 
 class Game
 
-  attr_accessor :players, :score, :status
+  attr_accessor :players, :player1, :player2, :current_player, :current_question
 
-  puts "Welcome to twO-O player math game! Who will be the first player to join the game?"
-  name1 = gets.chomp
-  player1 = Player.new(name1)
-  puts "Thanks for joining the game, #{player1.name}? Who else is playing?"
-  name2 = gets.chomp
-  player2 = Player.new(name2)
-  puts "Thanks for joining the game, #{player2.name}! Let's get started! \n#{player1.name}, you're up first. Here's the first question:"
+  def initialize
+    @player1 = ""
+    @player2 = ""
+    @players = [@player1, @player2]
+    @current_player = @player1
+    @current_question = []
+    @game_active = true
+  end
 
+  def set_question 
+    num1 = rand(21)
+    num2 = rand(21)
+    @current_question = [num1, num2]
+  end
+
+  def ask_question
+    @current_question = "What does #{current_question[0]} plus #{current_question[1]} equal?"
+  end
+
+  def are_lives_gone
+    players.any? {|player| player.lives == 0}
+  end
+
+  # def run_questions
+  #   until are_lives_gone()
+  #     set_question()
+  #     puts ask_question()
+  #   end
+  # end
+
+  def start_game
+    puts "Welcome to twO-O player math game! Who will be the first player to join the game?"
+    player1 = Player.new(gets.chomp)
+    puts "Thanks for joining the game, #{player1.name}? Who else is playing?"
+    player2 = Player.new(gets.chomp)
+    @players = [player1, player2]
+    
+    puts "Thanks for joining the game, #{players[0].name}! Let's get started! \n#{players[1].name}, you're up first.\nHere's your first question:"
+  end
 
 end
 
 
-game = Game.new
+def create_game
+  game = Game.new
+end
+
+game = Game.new()
+
+game.start_game
